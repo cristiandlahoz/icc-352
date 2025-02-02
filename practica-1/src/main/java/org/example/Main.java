1f89175bc4232a1363ee2174c68afd523826822f
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-02 08:27:18 -0400

    style(practica-1): format code with jdtls java lsp

--- practica-1/src/main/java/org/example/Main.java
+++ practica-1/src/main/java/org/example/Main.java
@@ -12,50 +12,70 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-
 public class Main {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Favor de introducir la URL: ");
-		String url = sc.nextLine();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Favor de introducir la URL: ");
+        String url = sc.nextLine();
 
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		String contentType = response.headers().firstValue("Content-Type").orElse("Unknown");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String contentType = response.headers().firstValue("Content-Type").orElse("Unknown");
 
-		if (contentType.contains("text/html")) {
-			Document document = Jsoup.parse(response.body());
+        if (contentType.contains("text/html")) {
+            Document document = Jsoup.parse(response.body());
 
-			System.out.println("Cantidad de lineas contenidad en el recurso retornado: " + response.body().split("\n").length);
+            System.out.println(
+                    "Cantidad de lineas contenidad en el recurso retornado: " + response.body().split("\n").length);
 
-			Elements paragraphs = document.select("p");
-			System.out.println("Cantidad de párrafos que contiene el documento:" + paragraphs.size());
+            Elements paragraphs = document.select("p");
+            System.out.println("Cantidad de párrafos que contiene el documento:" + paragraphs.size());
 
-			int imgInsideParagraphs = 0;
-			for (Element paragraph : paragraphs) {
-				imgInsideParagraphs += paragraph.select("img").size();
-			}
-			System.out.println("Cantidad de imagenes dentro de parrafos: " + imgInsideParagraphs);
-			Elements forms = document.select("form");
-			for (Element form : forms) {
-				String method = form.attr("method").toUpperCase();
+            int imgInsideParagraphs = 0;
+            for (Element paragraph : paragraphs) {
+                imgInsideParagraphs += paragraph.select("img").size();
+            }
+            System.out.println("Cantidad de imagenes dentro de parrafos: " + imgInsideParagraphs);
+            Elements forms = document.select("form");
+            for (Element form : forms) {
+                String method = form.attr("method").toUpperCase();
+                String action = form.attr("action");
 
-				System.out.println("Action del formulario tipo: " + method);
-				Elements inputs = form.select("input");
+                action = validarUrl(url, action);
+                System.out.println("Action del formulario tipo: " + method);
+                Elements inputs = form.select("input");
 
-				for (Element input : inputs) {
-					String campo = input.attr("name");
-					String tipo = input.attr("type");
-					System.out.println(("Campo del input dentro del formulario: " + campo + " tipo: " + tipo));
-				}
-				if (method.equals("POST")) {
-					HttpRequest postRequest = HttpRequest.newBuilder().uri(URI.create(url)).header("matricula-id", "10149779").POST(HttpRequest.BodyPublishers.ofString("asignatura=practica1")).build();
-					HttpResponse<String> postResponse = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
-					System.out.println("Respuesta a traves del formulario: " + postResponse.body());
-				}
-			}
-		}
-		sc.close();
-	}
+                for (Element input : inputs) {
+                    String campo = input.attr("name");
+                    String tipo = input.attr("type");
+                    System.out.println(("Campo del input dentro del formulario: " + campo + " tipo: " + tipo));
+                }
+                if (method.equals("POST")) {
+                    HttpRequest postRequest = HttpRequest.newBuilder().uri(URI.create(action))
+                            .header("matricula-id", "10149779")
+                            .header("Content-type", "application/x-www-form-urlencoded")
+                            .POST(HttpRequest.BodyPublishers.ofString("asignatura=practica1")).build();
+                    HttpResponse<String> postResponse = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
+                    System.out.println("Respuesta a traves del formulario: " + postResponse.body());
+                }
+            }
+        }
+        System.out.println("Tipo de documento: : " + contentType);
+        sc.close();
+    }
+
+    public static String validarUrl(String url, String action) {
+        if (!action.startsWith("http://") && !action.startsWith("https://")) {
+            if (url.endsWith("/") && !action.startsWith("/") || !url.endsWith("/") && action.startsWith("/"))
+                return action = url + action;
+            else if (url.endsWith("/") && action.startsWith("/")) {
+                StringBuilder sb = new StringBuilder(action);
+                sb.deleteCharAt(0);
+                return action = sb.toString();
+            } else
+                return action = url + "/" + action;
+        }
+        return action;
+    }
 }
