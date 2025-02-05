a70f18a7a47690f49a2d989c2a9f0bfa6121068a
Carolina <cbencome@gmail.com>
2025-02-05 12:27:49 -0400

    feat(practica-2): enhance ArticleController with CRUD operations for articles

--- practica-2/app/src/main/java/org/example/controllers/ArticleController.java
+++ practica-2/app/src/main/java/org/example/controllers/ArticleController.java
@@ -11,32 +11,35 @@ public class ArticleController {
     private static final ArticleService articleService = new ArticleService();
 
     public static void getAllArticles(Context ctx) {
-        Collection<Article> articles = articleService.getAllArticles();
-        ctx.json(articles);
+        Collection<Article> MyArticles = articleService.getAllArticles();
+        ctx.json(MyArticles);
+    }
+
+    public static void getArticleById(Context ctx) {
+        long id = Integer.parseInt(ctx.pathParam("id"));
+        Article myArticle = articleService.getArticleById(id);
+        ctx.json(myArticle);
     }
 
     public static void createArticle(Context ctx) {
-        Article article = ctx.bodyAsClass(Article.class);
-        articleService.createArticle(article);
+        Article myArticle = ctx.bodyAsClass(Article.class);
+        articleService.createArticle(myArticle);
         ctx.status(201);
     }
 
-    // public static void deleteArticle(Context ctx) {
-    // String idParam = ctx.pathParam("id");
-    //
-    // try {
-    // Long id = Long.parseLong(idParam);
-    //
-    // if (articles.containsKey(id)) {
-    // articles.remove(id);
-    // ctx.status(200).result("Artículo con ID " + id + " eliminado
-    // correctamente.");
-    // } else {
-    // ctx.status(404).result("Artículo con ID " + id + " no encontrado.");
-    // }
-    // } catch (NumberFormatException e) {
-    // ctx.status(400).result("ID inválido.");
-    // }
-    // }
+    public static void updateArticle(Context ctx) {
+        Article myArticle = ctx.bodyAsClass(Article.class);
+        String id = ctx.pathParam("id");
+        Long articleId = Long.parseLong(id);
+        myArticle.setArticleId(articleId);
+        articleService.updateArticle(myArticle);
+        ctx.status(200);
+    }
 
+    public static void deleteArticle(Context ctx) {
+        String id = ctx.pathParam("id");
+        Long articleId = Long.parseLong(id);
+        articleService.deleteArticleById(articleId);
+        ctx.status(200);
+    }
 }
