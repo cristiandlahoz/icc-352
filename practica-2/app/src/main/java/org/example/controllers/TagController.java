3f503cdd190396bdd34da5e1a8408aaaca6945ed
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-05 19:48:00 -0400

    feat(practica-2): add tag controller methods

--- practica-2/app/src/main/java/org/example/controllers/TagController.java
+++ practica-2/app/src/main/java/org/example/controllers/TagController.java
@@ -2,20 +2,38 @@ package org.example.controllers;
 
 import io.javalin.http.Context;
 import org.example.models.Tag;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.example.services.TagService;
 
 public class TagController {
-    private static List<Tag> tags = new ArrayList<>();
+    private static final TagService tagService = new TagService();
 
     public static void getAllTags(Context ctx) {
-        ctx.json(tags);
+        ctx.json(tagService);
+    }
+
+    public static void getTagById(Context ctx) {
+        String stringId = ctx.pathParam("id");
+        Long id = Long.parseLong(stringId);
+        Tag myTag = tagService.getTagById(id);
+        ctx.status(200).json(myTag);
     }
 
     public static void createTag(Context ctx) {
-        Tag tag = ctx.bodyAsClass(Tag.class);
-        tags.add(tag);
+        Tag myTag = ctx.bodyAsClass(Tag.class);
+        tagService.createTag(myTag);
         ctx.status(201);
+    }
+
+    public static void updateTag(Context ctx) {
+        Tag myTag = ctx.bodyAsClass(Tag.class);
+        tagService.updateTag(myTag);
+        ctx.status(200);
+    }
+
+    public static void deleteTag(Context ctx) {
+        String stringId = ctx.pathParam("id");
+        Long id = Long.parseLong(stringId);
+        tagService.deleteTagById(id);
+        ctx.status(200);
     }
 }
