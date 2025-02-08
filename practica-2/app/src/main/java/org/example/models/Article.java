0fb047420faa24b9cd55a6cd88a351c4f4a21adf
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-08 04:18:50 -0400

    refactor(practica-2): re order article title and content in signature constructor

--- practica-2/app/src/main/java/org/example/models/Article.java
+++ practica-2/app/src/main/java/org/example/models/Article.java
@@ -4,75 +4,75 @@ import java.util.Date;
 import java.util.ArrayList;
 
 public class Article {
-	private Long articleId;
-	private String articleTitle;
-	private String articleContent;
-	private User articleAuthor;
-	private Date articleDate;
-	private ArrayList<Comment> articleComments;
-	private ArrayList<Tag> articleTags;
+    private Long articleId;
+    private String articleTitle;
+    private String articleContent;
+    private User articleAuthor;
+    private Date articleDate;
+    private ArrayList<Comment> articleComments;
+    private ArrayList<Tag> articleTags;
 
-	public Article(Long articleId, String articleContent, String articleTitle, User articleAuthor, Date articleDate) {
-		this.articleId = articleId;
-		this.articleContent = articleContent;
-		this.articleTitle = articleTitle;
-		this.articleAuthor = articleAuthor;
-		this.articleDate = articleDate;
-	}
+    public Article(Long articleId, String articleTitle, String articleContent, User articleAuthor, Date articleDate) {
+        this.articleId = articleId;
+        this.articleTitle = articleTitle;
+        this.articleContent = articleContent;
+        this.articleAuthor = articleAuthor;
+        this.articleDate = articleDate;
+    }
 
-	public Long getArticleId() {
-		return articleId;
-	}
+    public Long getArticleId() {
+        return articleId;
+    }
 
-	public void setArticleId(Long articleId) {
-		this.articleId = articleId;
-	}
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
 
-	public String getArticleTitle() {
-		return articleTitle;
-	}
+    public String getArticleTitle() {
+        return articleTitle;
+    }
 
-	public void setArticleTitle(String articleTitle) {
-		this.articleTitle = articleTitle;
-	}
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
+    }
 
-	public String getArticleContent() {
-		return articleContent;
-	}
+    public String getArticleContent() {
+        return articleContent;
+    }
 
-	public void setArticleContent(String articleContent) {
-		this.articleContent = articleContent;
-	}
+    public void setArticleContent(String articleContent) {
+        this.articleContent = articleContent;
+    }
 
-	public User getArticleAuthor() {
-		return articleAuthor;
-	}
+    public User getArticleAuthor() {
+        return articleAuthor;
+    }
 
-	public void setArticleAuthor(User articleAuthor) {
-		this.articleAuthor = articleAuthor;
-	}
+    public void setArticleAuthor(User articleAuthor) {
+        this.articleAuthor = articleAuthor;
+    }
 
-	public java.util.Date getArticleDate() {
-		return articleDate;
-	}
+    public java.util.Date getArticleDate() {
+        return articleDate;
+    }
 
-	public void setArticleDate(Date articleDate) {
-		this.articleDate = articleDate;
-	}
+    public void setArticleDate(Date articleDate) {
+        this.articleDate = articleDate;
+    }
 
-	public ArrayList<Comment> getArticleComments() {
-		return articleComments;
-	}
+    public ArrayList<Comment> getArticleComments() {
+        return articleComments;
+    }
 
-	public void setArticleComments(ArrayList<Comment> articleComments) {
-		this.articleComments = articleComments;
-	}
+    public void setArticleComments(ArrayList<Comment> articleComments) {
+        this.articleComments = articleComments;
+    }
 
-	public ArrayList<Tag> getArticleTags() {
-		return articleTags;
-	}
+    public ArrayList<Tag> getArticleTags() {
+        return articleTags;
+    }
 
-	public void setArticleTags(ArrayList<Tag> articleTags) {
-		this.articleTags = articleTags;
-	}
+    public void setArticleTags(ArrayList<Tag> articleTags) {
+        this.articleTags = articleTags;
+    }
 }
