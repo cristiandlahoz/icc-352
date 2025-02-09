639d6a3546044161ad59694159ab4c56a8bb10b0
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-09 12:22:59 -0400

    refactor(practica-2): rename getUser and setUser methods to getUsername and setUsername in Comment model

--- practica-2/app/src/main/java/org/example/models/Comment.java
+++ practica-2/app/src/main/java/org/example/models/Comment.java
@@ -34,11 +34,11 @@ public class Comment {
 		this.comment = comment;
 	}
 
-	public String getUser() {
+	public String getUsername() {
 		return username;
 	}
 
-	public void setUser(String username) {
+	public void setUsername(String username) {
 		this.username = username;
 	}
 
