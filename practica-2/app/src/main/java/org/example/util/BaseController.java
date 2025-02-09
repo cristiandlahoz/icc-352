e58503b2be765eb81203a284a1847148086d0531
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-08 20:26:15 -0400

    feat(practica-2): add setModel method for flexible model setup in BaseController

--- practica-2/app/src/main/java/org/example/util/BaseController.java
+++ practica-2/app/src/main/java/org/example/util/BaseController.java
@@ -1,5 +1,8 @@
 package org.example.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.javalin.Javalin;
 
 public abstract class BaseController {
@@ -11,5 +14,21 @@ public abstract class BaseController {
     }
 
     abstract public void applyRoutes();
+
+    public Map<String, Object> setModel(Object... args) {
+        Map<String, Object> modelo = new HashMap<>();
+        if (args.length % 2 != 0) {
+            throw new IllegalArgumentException(
+                    "Invalid number of arguments. Arguments should be in pairs (key, value).");
+        }
+        for (int i = 0; i < args.length; i += 2) {
+            if (!(args[i] instanceof String)) {
+                throw new IllegalArgumentException(
+                        "Argument at position " + i + " is not a String. Keys should be Strings.");
+            }
+            modelo.put((String) args[i], args[i + 1]);
+        }
+        return modelo;
+    }
 
 }
