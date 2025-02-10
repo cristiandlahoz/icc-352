6d604b753f15ca307c4f3004ca3817d37dabb18d
Carolina <cbencome@gmail.com>
2025-02-10 09:10:38 -0400

    feat(practica-2): refactor UserController to extend BaseController and implement route handling

--- practica-2/app/src/main/java/org/example/controllers/UserController.java
+++ practica-2/app/src/main/java/org/example/controllers/UserController.java
@@ -1,13 +1,30 @@
 package org.example.controllers;
 
+import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.example.models.User;
 import org.example.services.UserService;
+import org.example.util.AccessStatus;
+import org.example.util.BaseController;
+import org.example.util.Role;
 
 import java.util.Collection;
 
-public class UserController {
+public class UserController extends BaseController {
     private static final UserService userService = new UserService();
+
+    public UserController(Javalin app) {
+        super(app);
+
+    }
+
+    public void applyRoutes() {
+        app.get("/users", UserController::getAllUsers);
+        app.get("/users/{username}", UserController::getUserByUsername);
+        app.post("/users/", UserController::createUser);
+        app.put("/users/{username}", UserController::updateUser);
+        app.delete("/users/{username}", UserController::deleteUser);
+    }
 
     public static void getAllUsers(Context ctx) {
         Collection<User> myUsers = userService.getAllUsers();
@@ -22,11 +39,42 @@ public class UserController {
     }
 
     public static void createUser(Context ctx) {
-        User myUser = ctx.bodyAsClass(User.class);
+       /* User myUser = ctx.bodyAsClass(User.class);
         userService.createUser(myUser);
-        ctx.status(201);
+        ctx.status(201);*/
+        String name = ctx.formParam("name");
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+        boolean isAuthor = ctx.formParam("is_author") != null;
+
+        if (name == null || username == null || password == null) {
+            ctx.redirect("/templates/create_user.html?error=missing_fields");
+            return;
+        }
+
+        try {
+            userService.getUserByUsername(username);
+            ctx.redirect("/templates/create_user.html?error=user_exists");
+            return;
+        } catch (IllegalArgumentException e) {
+            // Usuario no encontrado, se puede crear
+        }
+
+        Role role = isAuthor ? Role.AUTHOR : Role.USER;
+        User newUser = new User(username, name, password, role, AccessStatus.UNAUTHENTICATED);
+        userService.createUser(newUser);
+
+
+        if (newUser != null) {
+            ctx.status(201);
+            ctx.redirect("/");
+        } else {
+            ctx.status(400).result("Error creating user");
+        }
+
 
     }
+    /**/
 
     public static void updateUser(Context ctx) {
         User myUser = ctx.bodyAsClass(User.class);
