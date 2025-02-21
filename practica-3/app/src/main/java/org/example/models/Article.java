7be225e0287fb199e92afbe6814597665dc7fd40
Carolina <cbencome@gmail.com>
2025-02-21 07:07:19 -0400

    refactor(practica-3): simplify Article model and update relationships with comments and tags

--- practica-3/app/src/main/java/org/example/models/Article.java
+++ practica-3/app/src/main/java/org/example/models/Article.java
@@ -1,80 +1,32 @@
 package org.example.models;
 
-import java.util.Date;
-import java.util.ArrayList;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.*;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
-    private static Long size = 0L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long articleId;
     private String title;
     private String content;
     private String author;
     private Date date;
-    private ArrayList<Comment> comments;
-    private ArrayList<Tag> tags;
 
-    public Article( String title, String content, String author, Date date) {
-        size++;
-        this.articleId = size;
-        this.title = title;
-        this.content = content;
-        this.author = author;
-        this.date = date;
-    }
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
 
-    public Long getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(Long articleId) {
-        this.articleId = articleId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public java.util.Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public ArrayList<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(ArrayList<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public ArrayList<Tag> getTags() {
-        return tags;
-    }
-
-    public void setTags(ArrayList<Tag> tags) {
-        this.tags = tags;
-    }
+    @ManyToMany
+    @JoinTable(
+            name = "article_tags",
+            joinColumns = @JoinColumn(name = "article_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<Tag> tags;
 }
