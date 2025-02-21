fdac60aab6963b311b9dff9f48d98c8377295cdd
Carolina <cbencome@gmail.com>
2025-02-21 07:08:27 -0400

    refactor(practica-3): streamline Comment model by removing redundant fields and implementing JPA relationships

--- practica-3/app/src/main/java/org/example/models/Comment.java
+++ practica-3/app/src/main/java/org/example/models/Comment.java
@@ -1,59 +1,24 @@
 package org.example.models;
 
-
+import jakarta.persistence.*;
+import lombok.*;
 import java.util.*;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
-    private static Long size = 0L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long commentId;
 	private String comment;
 	private String username;
-	private Date date;
-	private Long articleId;
+	private Date date = new Date();
 
-	public Comment(String comment, String username, Long articleId) {
-        size++;
-		this.commentId = size;
-		this.comment = comment;
-		this.username = username;
-		this.date = new Date();
-		this.articleId = articleId;
-	}
-
-	public Long getCommentId() {
-		return commentId;
-	}
-
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public Long getArticleId() {
-		return articleId;
-	}
-
-	public void setArticleId(Long articleId) {
-		this.articleId = articleId;
-	}
+	@ManyToOne
+	@JoinColumn(name = "article_id", nullable = false)
+	private Article article;
 }
