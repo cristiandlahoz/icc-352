c11e115c09b51d5b055956ef0f3e68fba55f1843
Carolina <cbencome@gmail.com>
2025-02-21 07:08:53 -0400

    refactor(practica-3): simplify User model by removing manual methods and adding JPA annotations

--- practica-3/app/src/main/java/org/example/models/User.java
+++ practica-3/app/src/main/java/org/example/models/User.java
@@ -1,60 +1,27 @@
 package org.example.models;
 
+import jakarta.persistence.*;
+import lombok.*;
 import org.example.util.AccessStatus;
 import org.example.util.Role;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String username;
 	private String name;
 	private String password;
+
+	@Enumerated(EnumType.STRING)
 	private Role role;
+
+	@Enumerated(EnumType.STRING)
 	private AccessStatus accessStatus;
-
-	public User(String username, String name, String password, Role role, AccessStatus accessStatus) {
-		this.username = username;
-		this.name = name;
-		this.password = password;
-		this.role = role;
-		this.accessStatus = AccessStatus.UNAUTHENTICATED;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public AccessStatus isAccessStatus() {
-		return accessStatus;
-	}
-
-	public void setAccessStatus(AccessStatus accessStatus) {
-		this.accessStatus = accessStatus;
-	}
 }
