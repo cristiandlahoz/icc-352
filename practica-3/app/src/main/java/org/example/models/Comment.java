efca0dc7a62f9d73092ff288dde4f2b9b31a7f17
cristiandelahoz <cyrondelahoz@gmail.com>
2025-02-21 22:09:15 -0400

    feat(practica-3): enhance Comment model with author username and automatic date assignment

--- practica-3/app/src/main/java/org/example/models/Comment.java
+++ practica-3/app/src/main/java/org/example/models/Comment.java
@@ -2,23 +2,33 @@ package org.example.models;
 
 import jakarta.persistence.*;
 import lombok.*;
-import java.util.*;
+
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Comment {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long commentId;
 	private String comment;
-	private String username;
-	private Date date = new Date();
-
+	private String authorUsername;
+	private LocalDateTime date;
 	@ManyToOne
 	@JoinColumn(name = "article_id", nullable = false)
 	private Article article;
+
+	@PrePersist
+	protected void onCreate() {
+		this.date = LocalDateTime.now();
+	}
+
+	public Comment(String comment, String authorUsername, Article article) {
+		this.comment = comment;
+		this.authorUsername = authorUsername;
+		this.article = article;
+	}
 }
