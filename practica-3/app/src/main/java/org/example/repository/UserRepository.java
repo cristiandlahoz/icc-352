73c63355def6cab8e2eb795aad264d1897be6347
Carolina <cbencome@gmail.com>
2025-02-22 13:51:53 -0400

    feat(practica-3): add findByUsername method to UserRepository for user retrieval by username

--- practica-3/app/src/main/java/org/example/repository/UserRepository.java
+++ practica-3/app/src/main/java/org/example/repository/UserRepository.java
@@ -3,9 +3,17 @@ package org.example.repository;
 import jakarta.persistence.EntityManager;
 import org.example.models.User;
 import org.example.util.BaseRepository;
+import java.util.Optional;
 
-public class UserRepository extends BaseRepository<User, String> {
+public class UserRepository extends BaseRepository<User, Long> {
 	public UserRepository(EntityManager entityManager) {
 		super(entityManager, User.class);
+	}
+
+	public Optional<User> findByUsername(String username) {
+		return entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+				.setParameter("username", username)
+				.getResultStream()
+				.findFirst();
 	}
 }
