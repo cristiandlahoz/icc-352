24a13265370b978ebb9fda0d518fe1ce069ddbd8
Carolina <cbencome@gmail.com>
2025-02-24 03:13:09 -0400

    feat(practica-3): add PhotoRepository and PhotoService to DependencyConfig

--- practica-3/app/src/main/java/org/example/config/DependencyConfig.java
+++ practica-3/app/src/main/java/org/example/config/DependencyConfig.java
@@ -16,12 +16,15 @@ public class DependencyConfig {
   private static TagRepository tagRepository;
   private static UserRepository userRepository;
   private static AuthRepository authRepository;
+  private static PhotoRepository photoRepository;
 
   @Getter private static ArticleService articleService;
   @Getter private static AuthService authService;
   @Getter private static CommentService commentService;
   @Getter private static TagService tagService;
   @Getter private static UserService userService;
+  @Getter private static PhotoService photoService;
+
 
   public static void init() {
     entityManagerFactory = Persistence.createEntityManagerFactory("h2-persistence-unit");
@@ -31,6 +34,7 @@ public class DependencyConfig {
     commentRepository = new CommentRepository(entityManager);
     tagRepository = new TagRepository(entityManager);
     userRepository = new UserRepository(entityManager);
+    photoRepository = new PhotoRepository(entityManager);
     authRepository = new AuthRepository(CoackroachDBConfig.getSql2o());
 
     articleService = new ArticleService(articleRepository);
@@ -38,5 +42,8 @@ public class DependencyConfig {
     commentService = new CommentService(commentRepository, articleService);
     tagService = new TagService(tagRepository);
     userService = new UserService(userRepository);
+    photoService = new PhotoService(photoRepository);
   }
+
+
 }
