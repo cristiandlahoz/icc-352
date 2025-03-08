699ff70d4163f0c5c0f98ba1609a659b6c48386e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-08 01:48:22 -0400

    feat(asignacion-aula-2): add dependency inyection config

--- asignacion-aula-2/app/src/main/java/org/example/App.java
+++ asignacion-aula-2/app/src/main/java/org/example/App.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import org.example.controller.StudentController;
+import org.example.repository.StudentRepository;
+import org.example.service.StudentService;
+import org.example.util.BootStrap;
+import org.sql2o.Sql2o;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class App {
   public static void main(String[] args) {
@@ -10,8 +19,20 @@ public class App {
         cf.hostedPath = "/";
         cf.directory = "/public";
       });
+      ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+      templateResolver.setPrefix("/templates/");
+
+      TemplateEngine templateEngine = new TemplateEngine();
+      templateEngine.setTemplateResolver(templateResolver);
+
+      JavalinThymeleaf thymeleafRenderer = new JavalinThymeleaf(templateEngine);
+      config.fileRenderer(thymeleafRenderer);
     }).start(PORT);
 
-    app.get("/", ctx -> ctx.result("Hello World"));
+    BootStrap.getInstance().initDatabase();
+    Sql2o sql2o = BootStrap.getInstance().getSql2o();
+    StudentRepository studentRepository = new StudentRepository(sql2o);
+    StudentService studentService = new StudentService(studentRepository);
+    new StudentController(studentService, app).applyRoutes();
   }
 }
