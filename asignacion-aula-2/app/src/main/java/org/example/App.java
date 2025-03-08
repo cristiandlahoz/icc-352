abf822564271747a6851b8aabbba73c7236df6c7
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-07 20:09:34 -0400

    feat(asignacion-aula-2): add environment variable port management

--- asignacion-aula-2/app/src/main/java/org/example/App.java
+++ asignacion-aula-2/app/src/main/java/org/example/App.java
@@ -4,12 +4,13 @@ import io.javalin.Javalin;
 
 public class App {
   public static void main(String[] args) {
+    int PORT = System.getenv("PORT") != null ? Integer.parseInt(System.getenv("PORT")) : 7000;
     Javalin app = Javalin.create(config -> {
       config.staticFiles.add(cf -> {
         cf.hostedPath = "/";
         cf.directory = "/public";
       });
-    }).start(8088);
+    }).start(PORT);
 
     app.get("/", ctx -> ctx.result("Hello World"));
   }
