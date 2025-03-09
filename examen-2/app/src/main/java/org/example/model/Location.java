5ede3b19e7deda0bf9cff7897736a7c287ce40ad
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-09 13:37:07 -0400

    feat(examen-2): add Location model class

---
+++ examen-2/app/src/main/java/org/example/model/Location.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Location {
+  private Long id;
+  private Double latitude;
+  private Double longitude;
+}
