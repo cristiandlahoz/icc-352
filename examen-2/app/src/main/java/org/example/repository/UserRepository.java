ddf7f81a99344e043d28544b589810c1014e04b9
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-09 16:08:47 -0400

    feat(examen-2): add find and delete methods by username

--- examen-2/app/src/main/java/org/example/repository/UserRepository.java
+++ examen-2/app/src/main/java/org/example/repository/UserRepository.java
@@ -1,12 +1,38 @@
 package org.example.repository;
 
+import java.util.Optional;
+
 import org.example.model.User;
 import org.example.util.baseclasses.BaseRepository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class UserRepository extends BaseRepository<User, Long> {
   public UserRepository(EntityManager entityManager) {
     super(entityManager, User.class);
+  }
+
+  public Optional<User> findByUsername(String username) {
+    return entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+        .setParameter("username", username)
+        .getResultList()
+        .stream()
+        .findFirst();
+  }
+
+  public void deleteByUsername(String username) {
+    EntityTransaction transaction = entityManager.getTransaction();
+
+    try {
+      transaction.begin();
+      entityManager.createQuery("DELETE FROM User u WHERE u.username = :username")
+          .setParameter("username", username)
+          .executeUpdate();
+      transaction.commit();
+    } catch (Exception e) {
+      transaction.rollback();
+      throw new RuntimeException("Error deleting user by username: " + username, e);
+    }
   }
 }
