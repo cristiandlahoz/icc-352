a3bb631745d46ae64c672a00bb1e7735f20a524b
Carolina <cbencome@gmail.com>
2025-03-13 01:48:57 -0400

    feat(examen-2): create FormDTO record for form data handling

---
+++ examen-2/app/src/main/java/org/example/dto/FormDTO.java
@@ -0,0 +1,11 @@
+package org.example.dto;
+
+public record FormDTO(
+        String username,
+        String fullname,
+        String sector,
+        String education,
+        Double latitude,
+        Double longitude,
+        Boolean isSynchronized
+) {}
