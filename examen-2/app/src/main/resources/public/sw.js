98886592b90cd2904e303aaa22990eabe83f3b4e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-14 10:41:37 -0400

    feat(service-worker): add service worker for caching static assets and offline support

---
+++ examen-2/app/src/main/resources/public/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/forms/create',
+  '/js/offline.js',
+  '/sw.js',
+];
+
+// Install event: Cache static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Caching static assets');
+        return cache.addAll(STATIC_ASSETS);
+      })
+  );
+});
+
+// Fetch event: Serve from cache if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (event.request.method === 'POST') {
+          return fetch(event.request);
+        } else {
+          console.log(`method: ${event.request.method}`);
+          console.log(`Serving ${event.request.url} from cache`);
+          return response || fetch(event.request);
+        }
+      }).catch((error) => {
+        console.error('Error in fetch handler:', error);
+      }))
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
