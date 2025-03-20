be154d58b9abd71094c2480eb9c7befb11ea2e59
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-20 00:53:09 -0400

    feat(p5): add ChatRoom interface for managing chat room functionality

---
+++ practica-5/app/src/main/java/org/example/interfaces/ChatRoom.java
@@ -0,0 +1,11 @@
+package org.example.interfaces;
+
+import io.javalin.websocket.WsContext;
+
+public interface ChatRoom {
+  void sendMessage(String message, String username);
+
+  void addUser(String username, WsContext ctx);
+
+  void removeUser(String username);
+}
