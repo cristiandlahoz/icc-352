79351128552878f542f1e3412e39a0dbe127b838
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-22 11:32:31 -0400

    refactor(p5:room): clean up sendMessage method and remove commented code

--- practica-5/app/src/main/java/org/example/util/Room.java
+++ practica-5/app/src/main/java/org/example/util/Room.java
@@ -10,15 +10,6 @@ import io.javalin.websocket.WsContext;
 public class Room implements ChatRoom {
   private final Map<String, WsContext> users = new ConcurrentHashMap<>();
 
- /* @Override
-  public void sendMessage(String message, String username) {
-    users.values().forEach(ctx -> {
-      if (ctx.session.isOpen() && !ctx.queryParam("user").equals(username)) {
-        ctx.send(message);
-      }
-    });
-  }*/
-
   @Override
   public void sendMessage(String message, String username) {
     users.values().forEach(ctx -> {
@@ -36,28 +27,29 @@ public class Room implements ChatRoom {
     if (users.size() >= 2) {
       ctx.send("Chat is full");
       ctx.session.close();
-      return false;  // ❌ No se pudo agregar
+      return false; // ❌ No se pudo agregar
     }
 
     if (users.containsKey(username)) {
       ctx.send("El usuario ya está en la sala.");
-      return false;  // ❌ Usuario ya estaba en la sala
+      return false; // ❌ Usuario ya estaba en la sala
     }
 
     users.put(username, ctx);
-    return true;  // ✅ Usuario agregado correctamente
+    return true; // ✅ Usuario agregado correctamente
   }
 
-
-  /*@Override
-  public void addUser(String username, WsContext ctx) {
-    if (users.size() >= 2) {
-      ctx.send("Chat is full");
-      ctx.session.close();
-      return;
-    }
-    users.put(username, ctx);
-  }*/
+  /*
+   * @Override
+   * public void addUser(String username, WsContext ctx) {
+   * if (users.size() >= 2) {
+   * ctx.send("Chat is full");
+   * ctx.session.close();
+   * return;
+   * }
+   * users.put(username, ctx);
+   * }
+   */
 
   @Override
   public void removeUser(String username) {
