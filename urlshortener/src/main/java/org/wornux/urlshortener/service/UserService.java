a47cc5f2988fcc7500c395a9b9a904021af94203
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 17:07:19 -0400

    feat(service): implement UserService for user management operations

--- urlshortener/src/main/java/org/wornux/urlshortener/service/UserService.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/service/UserService.java
@@ -1,1 +1,44 @@
+package org.wornux.urlshortener.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
+
+import org.bson.types.ObjectId;
+import org.wornux.urlshortener.dao.UserDAO;
+import org.wornux.urlshortener.model.User;
+
+public class UserService {
+
+  private final UserDAO userDAO;
+
+  public UserService(@Nonnull UserDAO userDAO) {
+    this.userDAO = userDAO;
+  }
+
+  public Optional<User> authenticate(@Nonnull String username, @Nonnull String password) {
+    return userDAO.findByUsernameAndPassword(username, password);
+  }
+
+  public List<User> getUsers() {
+    return userDAO.findAll();
+  }
+
+  public Optional<User> getUserById(@Nonnull ObjectId id) {
+    return userDAO.findById(id);
+  }
+
+  public void saveUser(@Nonnull User user) {
+    userDAO.save(user);
+  }
+
+  public void updateUser(@Nonnull User user) {
+    userDAO.update(user);
+  }
+
+  public void deleteUser(@Nonnull ObjectId id) {
+    userDAO.deleteById(id);
+  }
+
+}
