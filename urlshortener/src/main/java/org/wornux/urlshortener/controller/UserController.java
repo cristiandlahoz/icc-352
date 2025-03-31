6f6797f701b6d04e39eb3a2520ae93c3b9ad3df1
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 16:35:19 -0400

    feat(controller): implement UserController with endpoint to list users

--- urlshortener/src/main/java/org/wornux/urlshortener/controller/UserController.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/controller/UserController.java
@@ -1,1 +1,26 @@
+package org.wornux.urlshortener.controller;
 
+import javax.annotation.Nonnull;
+
+import org.wornux.urlshortener.controller.base.BaseController;
+import org.wornux.urlshortener.enums.Routes;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class UserController extends BaseController {
+
+  public UserController(Javalin app) {
+    super(app);
+  }
+
+  @Override
+  public void mapEndpoints() {
+    app.get(Routes.USER_LIST.getRoute(), this::getUsers);
+  }
+
+  public void getUsers(@Nonnull Context ctx) {
+    ctx.result("Users");
+  }
+
+}
