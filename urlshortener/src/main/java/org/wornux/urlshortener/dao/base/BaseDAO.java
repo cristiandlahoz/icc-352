3e6e35f58701eb91009b5f77fe5f84febb722132
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 16:35:32 -0400

    feat(dao): add BaseDAO for generic CRUD operations with Morphia

---
+++ urlshortener/src/main/java/org/wornux/urlshortener/dao/base/BaseDAO.java
@@ -0,0 +1,80 @@
+package org.wornux.urlshortener.dao.base;
+
+import dev.morphia.Datastore;
+import dev.morphia.query.Query;
+import dev.morphia.query.filters.Filters;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.mongodb.lang.NonNull;
+
+/**
+ * A generic base repository implementation for Morphia.
+ * Provides common CRUD operations for entities.
+ *
+ * @param <T> The type of the entity.
+ * @param <K> The type of the entity's ID.
+ */
+public abstract class BaseDAO<T, K> {
+  private final Datastore datastore;
+  private final Class<T> entityClass;
+
+  /**
+   * Constructs a new BaseRepository.
+   *
+   * @param datastore   The Morphia Datastore instance.
+   * @param entityClass The class of the entity this repository manages.
+   */
+  public BaseDAO(@NonNull Datastore datastore, @NonNull Class<T> entityClass) {
+    this.datastore = datastore;
+    this.entityClass = entityClass;
+  }
+
+  /**
+   * Finds an entity by its ID.
+   *
+   * @param id The ID of the entity to find.
+   * @return An Optional containing the entity if found, or empty if not found.
+   */
+  public Optional<T> findById(@NonNull K id) {
+    return Optional.ofNullable(datastore.find(entityClass).filter(Filters.eq("_id", id)).first());
+  }
+
+  /**
+   * Retrieves all entities of the specified type.
+   *
+   * @return A list of all entities.
+   */
+  public List<T> findAll() {
+    Query<T> query = datastore.find(entityClass);
+    return query.iterator().toList();
+  }
+
+  /**
+   * Saves or updates an entity in the datastore.
+   *
+   * @param entity The entity to save or update.
+   */
+  public void save(@NonNull T entity) {
+    datastore.save(entity);
+  }
+
+  /**
+   * Updates an entity in the datastore.
+   *
+   * @param entity The entity to update.
+   */
+  public void update(@NonNull T entity) {
+    datastore.save(entity);
+  }
+
+  /**
+   * Deletes an entity by its ID.
+   *
+   * @param id The ID of the entity to delete.
+   */
+  public void deleteById(@NonNull K id) {
+    datastore.find(entityClass).filter(Filters.eq("_id", id)).delete();
+  }
+}
