8ea80e7c3508d22387d6fb92156b0ee766bd1cd6
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 01:19:51 -0400

    feat(enum): add SecurityConstants for JWT configuration

---
+++ urlshortener/src/main/java/org/wornux/urlshortener/enums/SecurityConstants.java
@@ -0,0 +1,29 @@
+package org.wornux.urlshortener.enums;
+
+public enum SecurityConstants {
+
+  /**
+   * The secret key used for signing JWT tokens.
+   */
+  JWT_SECRET("A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5Z6"),
+
+  /**
+   * The expiration time for JWT tokens in milliseconds.
+   */
+  JWT_EXPIRATION_TIME(3_600_000L); // 1 hour
+
+  private final Object value;
+
+  SecurityConstants(Object value) {
+    this.value = value;
+  }
+
+  /**
+   * Retrieves the value of the constant.
+   *
+   * @return the value of the constant
+   */
+  public Object getValue() {
+    return value;
+  }
+}
