c3f2b8a59a906a43d69414c684f2c96f1adfa817
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 01:17:20 -0400

    refactor(pf): update AppConfig to use JavalinConfig and integrate Thymeleaf for template rendering

--- urlshortener/src/main/java/org/wornux/urlshortener/config/AppConfig.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/config/AppConfig.java
@@ -1,10 +1,35 @@
 package org.wornux.urlshortener.config;
 
-import io.javalin.config.Config;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class AppConfig {
-  public static void configureApp(Config config) {
-    // Example: Enable CORS, set default error handling, etc.
-    config.enableCorsForAllOrigins();
+  public static void configureApp(JavalinConfig config) {
+    config.staticFiles.add(stc -> {
+      stc.hostedPath = "/";
+      stc.directory = "/public";
+    });
+    config.fileRenderer(templateEngineConfig());
+  }
+
+  public static JavalinThymeleaf templateEngineConfig() {
+    ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+    templateResolver.setPrefix("/templates/");
+
+    TemplateEngine templateEngine = new TemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver);
+
+    JavalinThymeleaf thymeleafRenderer = new JavalinThymeleaf(templateEngine);
+    return thymeleafRenderer;
+  }
+
+  public static void ConfigureExceptionHandlers(Javalin app) {
+    app.error(404, ctx -> {
+      ctx.render("pages/404.html");
+    });
   }
 }
