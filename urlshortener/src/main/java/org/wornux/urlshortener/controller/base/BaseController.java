97bcc80a0a9ad66560881982da5d959d017139de
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 18:12:37 -0400

    refactor(controller): remove Javalin instance from BaseController and clean up comments

--- urlshortener/src/main/java/org/wornux/urlshortener/controller/base/BaseController.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/controller/base/BaseController.java
@@ -3,36 +3,23 @@ package org.wornux.urlshortener.controller.base;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.javalin.Javalin;
-
 /**
  * BaseController serves as an abstract base class for all controllers in the
  * application.
  * It provides common functionality and structure for derived controllers.
  */
 public abstract class BaseController {
-  /**
-   * The Javalin application instance used to configure and manage HTTP routes.
-   * Marked as transient to avoid serialization issues, as Javalin is not
-   * serializable.
-   */
-  protected transient Javalin app;
-
-  public BaseController(Javalin app) {
-    this.app = app;
-  }
-
-  public abstract void mapEndpoints();
-
-  /**
+  /*
    * Creates a model map from the provided key-value pairs.
    * 
    * @param args A variable-length argument list where arguments should be in
-   *             pairs (key, value).
-   *             Keys must be Strings, and values can be any Object.
+   * pairs (key, value).
+   * Keys must be Strings, and values can be any Object.
+   * 
    * @return A Map<String, Object> containing the key-value pairs.
+   * 
    * @throws IllegalArgumentException If the number of arguments is not even or if
-   *                                  a key is not a String.
+   * a key is not a String.
    */
   public static Map<String, Object> setModel(Object... args) {
     Map<String, Object> model = new HashMap<>();
