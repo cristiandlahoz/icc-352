c732f439ca52abb10124b5c2a0bcd634bcf56dac
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-31 01:20:59 -0400

    feat(pf:jwt): add jwt implementation

--- urlshortener/src/main/java/org/wornux/urlshortener/security/JwtUtil.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/security/JwtUtil.java
@@ -1,27 +1,69 @@
 package org.wornux.urlshortener.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.wornux.urlshortener.dto.Authentication;
+import org.wornux.urlshortener.enums.SecurityConstants;
+import org.wornux.urlshortener.exception.AuthenticationCredentialsNotFoundException;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtUtil {
-  private static final String SECRET_KEY = "your-secret-key";
 
-  public static String generateToken(String username) {
+  public String generateToken(Map<String, String> claims, Authentication authentication) {
+    String username = authentication.username();
+    Date currentDate = new Date(System.currentTimeMillis());
+    long expirationTime = (long) SecurityConstants.JWT_EXPIRATION_TIME.getValue();
+    Date expirationDate = new Date(currentDate.getTime() + expirationTime);
+
     return Jwts.builder()
-        .setSubject(username)
-        .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour
-        .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+        .subject(username)
+        .claims(claims)
+        .issuedAt(currentDate)
+        .expiration(expirationDate)
+        .signWith(getSigningKey())
         .compact();
   }
 
-  public static boolean validateToken(String token) {
+  public boolean validateToken(String token) {
     try {
-      Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+      SecretKey key = getSigningKey();
+      Jwts.parser().decryptWith(key).build().parse(token);
       return true;
-    } catch (Exception e) {
-      return false;
+    } catch (SecurityException | MalformedJwtException e) {
+      throw new AuthenticationCredentialsNotFoundException("JWT was expired or incorrect");
+    } catch (ExpiredJwtException e) {
+      throw new AuthenticationCredentialsNotFoundException("Expired JWT token.");
+    } catch (UnsupportedJwtException e) {
+      throw new AuthenticationCredentialsNotFoundException("Unsupported JWT token.");
+    } catch (IllegalArgumentException e) {
+      throw new AuthenticationCredentialsNotFoundException("JWT token compact of handler are invalid.");
     }
+  }
+
+  public String getUsernameFromToken(String token) {
+    return extractAllClaims(token).getSubject();
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts.parser()
+        .verifyWith(getSigningKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  private SecretKey getSigningKey() {
+    String SECRET_KEY = (String) SecurityConstants.JWT_SECRET.getValue();
+    byte[] keyBytes = SECRET_KEY.getBytes();
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 }
