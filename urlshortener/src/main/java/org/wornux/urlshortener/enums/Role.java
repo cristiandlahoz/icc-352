151dc063b85f8ccbe5a0b80aef98ceae1b770741
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-01 15:53:21 -0400

    feat: add authmiddleware

--- urlshortener/src/main/java/org/wornux/urlshortener/enums/Role.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/enums/Role.java
@@ -1,16 +1,14 @@
 package org.wornux.urlshortener.enums;
 
+import io.javalin.security.RouteRole;
+
 /**
  * Represents the roles available in the URL shortener application.
  */
-public enum Role {
-  /**
-   * Role for administrators with elevated privileges.
-   */
+public enum Role implements RouteRole {
   ADMIN,
-
-  /**
-   * Role for regular users with standard access.
-   */
-  USER
+  USER,
+  USER_READ,
+  USER_WRITE,
+  ANYONE;
 }
