b2ef0c76c993c6b27c004031d8d88ed7acf815fa
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-02 14:26:12 -0400

    feat: enhance AuthMiddleware for improved authentication and authorization handling

--- urlshortener/src/main/java/org/wornux/urlshortener/security/AuthMiddleware.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/security/AuthMiddleware.java
@@ -4,23 +4,36 @@ import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.wornux.urlshortener.enums.Role;
 
+/**
+ * Middleware for handling authentication and authorization in the application.
+ * Ensures that the user is authenticated and has the required role to access
+ * specific resources.
+ */
 public class AuthMiddleware implements Handler {
 
+  /**
+   * Handles the authentication middleware logic.
+   * 
+   * @param ctx The context of the current request, providing access to request
+   *            and response data.
+   * @throws Exception If an error occurs during the handling of the request.
+   */
   @Override
   public void handle(Context ctx) throws Exception {
-    var permittedRoles = ctx.routeRoles();
-    // Check if the user is authenticated
-    if (ctx.basicAuthCredentials() != null) {
-      // Check if the user has the required role
-      if (permittedRoles.contains(Role.ADMIN)) {
-        // User is authenticated and has the required role
-      } else {
-        // User is authenticated but does not have the required role
-        ctx.status(403).result("Forbidden");
-      }
-    } else {
-      // User is not authenticated
-      ctx.status(401).result("Unauthorized");
+    if (ctx.path().equals("/"))
+      ctx.redirect("shortened");
+
+    if (ctx.sessionAttribute("user") == null) {
+      ctx.status(401); // Unauthorized
+      return;
+    }
+
+    // Retrieve the user's role from the session and check if they have admin
+    // privileges.
+    Role role = ctx.sessionAttribute("user");
+    if (role != Role.ADMIN) {
+      ctx.status(403); // Forbidden
+      return;
     }
   }
 }
