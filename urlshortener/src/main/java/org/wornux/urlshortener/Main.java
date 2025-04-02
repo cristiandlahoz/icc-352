4c84d6d362279289fb0cbf18f05e2572d9ec8425
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-01 22:40:03 -0400

    feat: add dotenv

--- urlshortener/src/main/java/org/wornux/urlshortener/Main.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/Main.java
@@ -2,13 +2,15 @@ package org.wornux.urlshortener;
 
 import org.wornux.urlshortener.config.AppConfig;
 import org.wornux.urlshortener.core.routing.Router;
+import org.wornux.urlshortener.util.EnvReader;
 
 import io.javalin.Javalin;
 
 public class Main {
   public static void main(String[] args) {
+    int PORT = EnvReader.getInt("PORT", 7_0_0_0);
     Javalin app = Javalin.create(
-        config -> AppConfig.configureApp(config)).start(7000);
+        config -> AppConfig.configureApp(config)).start(PORT);
 
     AppConfig.ConfigureExceptionHandlers(app);
     Router.registerRoutes(app);
