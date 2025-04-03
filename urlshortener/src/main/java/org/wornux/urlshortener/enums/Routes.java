3bfd3d061f2a6c1742b1bcad62aa23cd6f3bde47
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-02 23:39:07 -0400

    refactor(routes): update user login route and modify home route path

--- urlshortener/src/main/java/org/wornux/urlshortener/enums/Routes.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/enums/Routes.java
@@ -2,25 +2,17 @@ package org.wornux.urlshortener.enums;
 
 public enum Routes {
   // User routes
-  USER_LOGIN("/login"),
+  USER_LOGIN("/auth/login"),
   USER_REGISTER("/register"),
   USER_LOGOUT("/logout"),
-  USER_PROFILE("/profile"),
-  USER_UPDATE_PROFILE("/profile/update"),
-  USER_DELETE_PROFILE("/profile/delete"),
   USER_LIST("/users"),
   USER_VIEW("/users/{id}"),
   USER_UPDATE("/users/{id}/update"),
   USER_DELETE("/users/{id}/delete"),
-  // URL routes
-  URL_LIST("/urls"),
-  URL_CREATE("/urls/create"),
-  URL_VIEW("/urls/{id}"),
-  URL_UPDATE("/urls/{id}/update"),
-  URL_DELETE("/urls/{id}/delete"),
   // Redirect routes
   REDIRECT("/r/{id}"),
-  HOME("/");
+  HOME("/shortened");
+  // statics
 
   private final String route;
 
