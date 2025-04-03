2f3740141a169e13fff0eb09e8d75b8308be548b
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-03 11:34:02 -0400

    feat: add QR code generation page with Thymeleaf integration

--- urlshortener/src/main/java/org/wornux/urlshortener/util/QRCodeGenerator.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/util/QRCodeGenerator.java
@@ -4,9 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 
 /**
  * Utility class for generating QR codes.
