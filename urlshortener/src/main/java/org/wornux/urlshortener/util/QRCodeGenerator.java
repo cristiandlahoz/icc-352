8aad7d6a5c9e1b2f974bf377759c23899f4311c4
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-02 23:39:48 -0400

    feat(util): enhance QR code generation to return byte array and handle exceptions

--- urlshortener/src/main/java/org/wornux/urlshortener/util/QRCodeGenerator.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/util/QRCodeGenerator.java
@@ -1,17 +1,37 @@
 package org.wornux.urlshortener.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 
+/**
+ * Utility class for generating QR codes.
+ */
 public class QRCodeGenerator {
-  public static BitMatrix generateQRCode(String url) {
+
+  /**
+   * Generates a QR code for the given URL and returns it as a byte array.
+   *
+   * @param url The URL to encode in the QR code.
+   * @return A byte array representing the QR code image.
+   * @throws IOException If an error occurs during QR code generation.
+   */
+  public static byte[] generateQRCode(String url) throws IOException {
     QRCodeWriter writer = new QRCodeWriter();
+    int width = 200;
+    int height = 200;
     try {
-      return writer.encode(url, BarcodeFormat.QR_CODE, 200, 200);
+      BitMatrix bitMatrix = writer.encode(url, BarcodeFormat.QR_CODE, width, height);
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+      // Convert the ByteArrayOutputStream to a byte array
+      return outputStream.toByteArray();
     } catch (Exception e) {
-      e.printStackTrace();
-      return null;
+      throw new IOException("Failed to generate QR code", e);
     }
   }
 }
