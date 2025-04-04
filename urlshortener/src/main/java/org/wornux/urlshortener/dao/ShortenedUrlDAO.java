8a64af935c10adb9cf41a0f0689d200a3f4ff460
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-03 23:41:23 -0400

    feat: add findByHash method to ShortenedUrlDAO for retrieving URLs by shortened hash

--- urlshortener/src/main/java/org/wornux/urlshortener/dao/ShortenedUrlDAO.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/dao/ShortenedUrlDAO.java
@@ -1,14 +1,23 @@
 package org.wornux.urlshortener.dao;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.wornux.urlshortener.dao.base.BaseDAO;
 import org.wornux.urlshortener.model.ShortenedUrl;
 
 import dev.morphia.Datastore;
+import dev.morphia.query.filters.Filters;
 
 public class ShortenedUrlDAO extends BaseDAO<ShortenedUrl, ObjectId> {
 
   public ShortenedUrlDAO(Datastore datastore) {
     super(datastore, ShortenedUrl.class);
+  }
+
+  public Optional<ShortenedUrl> findByHash(String shortenedUrl) {
+    return Optional.ofNullable(datastore.find(ShortenedUrl.class)
+        .filter(Filters.eq("shortenedUrl", shortenedUrl))
+        .first());
   }
 }
