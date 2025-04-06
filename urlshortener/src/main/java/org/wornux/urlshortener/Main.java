ff01c9ba2b596fc37e7f2ef96b94eb214506de4e
Carolina <cbencome@gmail.com>
2025-04-05 21:41:35 -0400

    refactor(main): simplify main method by removing unused configurations and focusing on REST initialization

--- urlshortener/src/main/java/org/wornux/urlshortener/Main.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/Main.java
@@ -1,20 +1,20 @@
 package org.wornux.urlshortener;
 
-import org.wornux.urlshortener.config.AppConfig;
-import org.wornux.urlshortener.core.routing.Router;
-import org.wornux.urlshortener.util.EnvReader;
+import org.wornux.urlshortener.api.rest.Router;
 
-import io.javalin.Javalin;
 
 public class Main {
   public static void main(String[] args) {
-    int PORT = EnvReader.getInt("PORT", 7_0_0_0);
-    Javalin app = Javalin.create(
-        config -> AppConfig.configureApp(config)).start(PORT);
+    // Iniciar REST en su propio hilo
+    new Thread(Router::start).start();
 
-    AppConfig.ConfigureExceptionHandlers(app);
-    Router.registerRoutes(app);
-
-    app.get("/", ctx -> ctx.render("pages/home.html"));
+    // Iniciar gRPC en su propio hilo
+    /*new Thread(() -> {
+      try {
+        GrpcServer.start(9090);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();*/
   }
 }
