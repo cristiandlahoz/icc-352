04cb83fa67d655322f25f1d348b1097829477ebb
Carolina <cbencome@gmail.com>
2025-04-12 16:24:54 -0400

    refactor(UrlDAO): add findByCreatedBy method to retrieve URLs by user

--- urlshortener/src/main/java/org/wornux/urlshortener/dao/UrlDAO.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/dao/UrlDAO.java
@@ -2,10 +2,13 @@ package org.wornux.urlshortener.dao;
 
 import dev.morphia.Datastore;
 import dev.morphia.query.filters.Filters;
+
+import java.util.List;
 import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.wornux.urlshortener.dao.base.BaseDAO;
 import org.wornux.urlshortener.model.Url;
+import org.wornux.urlshortener.model.User;
 
 public class UrlDAO extends BaseDAO<Url, ObjectId> {
 
@@ -16,5 +19,12 @@ public class UrlDAO extends BaseDAO<Url, ObjectId> {
   public Optional<Url> findByHash(String shortenedUrl) {
     return Optional.ofNullable(
         datastore.find(Url.class).filter(Filters.eq("shortenedUrl", shortenedUrl)).first());
+  }
+
+  public List<Url> findByCreatedBy(User user) {
+    return datastore.find(Url.class)
+            .filter(Filters.eq("createdBy", user))
+            .iterator()
+            .toList();
   }
 }
