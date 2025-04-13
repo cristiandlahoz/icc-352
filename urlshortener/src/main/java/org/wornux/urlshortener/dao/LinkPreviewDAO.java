bf899abb1bc8d78ae492f4c267b37347454d6d02
Carolina <cbencome@gmail.com>
2025-04-13 00:10:30 -0400

    feat(LinkPreviewDAO): implement LinkPreviewDAO to manage LinkPreview entities

---
+++ urlshortener/src/main/java/org/wornux/urlshortener/dao/LinkPreviewDAO.java
@@ -0,0 +1,14 @@
+package org.wornux.urlshortener.dao;
+
+
+import dev.morphia.Datastore;
+import org.bson.types.ObjectId;
+import org.wornux.urlshortener.dao.base.BaseDAO;
+import org.wornux.urlshortener.model.LinkPreview;
+
+public class LinkPreviewDAO extends BaseDAO<LinkPreview, ObjectId> {
+
+    public LinkPreviewDAO(Datastore datastore) {
+        super(datastore, LinkPreview.class);
+    }
+}
