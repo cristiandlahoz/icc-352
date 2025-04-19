271bd9dee8a8b005f81ec01d1fb7e45c6b019efc
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-18 22:45:51 -0400

    feat(web:usercontroller): add user render page handler

--- urlshortener/src/main/java/org/wornux/urlshortener/controller/UserController.java
+++ urlshortener/src/main/java/org/wornux/urlshortener/controller/UserController.java
@@ -1,9 +1,16 @@
 package org.wornux.urlshortener.controller;
 
 import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.Nonnull;
 import org.wornux.urlshortener.core.routing.annotations.CONTROLLER;
 import org.wornux.urlshortener.core.routing.annotations.GET;
+import org.wornux.urlshortener.enums.Role;
+import org.wornux.urlshortener.enums.Routes;
+import org.wornux.urlshortener.enums.SessionKeys;
+import org.wornux.urlshortener.model.User;
 import org.wornux.urlshortener.service.UserService;
 
 /** UserController is a controller class that handles HTTP requests related to users. */
@@ -22,6 +29,21 @@ public class UserController {
    */
   @GET(path = "/")
   public void getUsers(@Nonnull Context ctx) {
-    ctx.result("Users");
+    User user = ctx.sessionAttribute(SessionKeys.USER.getKey());
+    if (user == null) {
+      ctx.redirect(Routes.USER_LOGIN.getRoute());
+      return;
+    } else if (!user.getRole().equals(Role.ADMIN)) {
+      ctx.status(HttpStatus.FORBIDDEN.getCode());
+      return;
+    }
+    Map<String, Object> model =
+        new HashMap<>() {
+          {
+            put("user", user);
+            put("users", userService.getUsers());
+          }
+        };
+    ctx.render("pages/users.html", model);
   }
 }
