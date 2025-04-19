aa7a61e06c56552a21bc5c61485679552557e520
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-18 22:46:09 -0400

    feat(js): add getChart script

---
+++ urlshortener/src/main/resources/public/js/getChart.js
@@ -0,0 +1,60 @@
+const getAnalyticsData = async () => {
+	const userEmail = document.getElementById("userEmail").textContent.trim();
+
+	try {
+		const response = await axios.get(`analyticsSender/${userEmail}`);
+		const analyticsData = response.data;
+
+		const chartData = {
+			labels: Object.keys(analyticsData),
+			datasets: [
+				{
+					label: "Clicks per day and hour",
+					data: Object.values(analyticsData),
+					backgroundColor: getBackgroundColors(
+						Object.keys(analyticsData).length,
+					),
+					borderColor: getBorderColors(Object.keys(analyticsData).length),
+					borderWidth: 1,
+				},
+			],
+		};
+
+		const ctx = document.getElementById("myChart").getContext("2d");
+		const config = {
+			type: "bar",
+			data: chartData,
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true,
+					},
+				},
+			},
+		};
+		new Chart(ctx, config);
+	} catch (error) {
+		console.error("Error at loading the data:", error);
+	}
+};
+
+const getBackgroundColors = (numColors) => {
+	const colors = [];
+	for (let i = 0; i < numColors; i++) {
+		const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.6)`;
+		colors.push(color);
+	}
+	return colors;
+};
+const getBorderColors = (numColors) => {
+	const colors = [];
+	for (let i = 0; i < numColors; i++) {
+		const backgroundColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.6)`;
+		// Convertir el color de fondo a un color ligeramente más oscuro para el borde
+		const borderColor = backgroundColor.replace(/[^,]+(?=\))/, "0.6)");
+		colors.push(borderColor);
+	}
+	return colors;
+};
+
+document.addEventListener("DOMContentLoaded", getAnalyticsData);
