1e8c4bfbd6909b73159e67a2297b5737dca3515e
Carolina <cbencome@gmail.com>
2025-04-21 14:32:16 -0400

    refactor(sw.js): update service worker registration and cache management with dynamic name

--- urlshortener/src/main/resources/public/sw.js
+++ urlshortener/src/main/resources/public/sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'wornux-cache-v1';
+/*const CACHE_NAME = 'wornux-cache-v1';
+
 const STATIC_ASSETS = [
     '/shortened',
     '/shortened/dashboard',
@@ -11,6 +12,8 @@ const STATIC_ASSETS = [
 
 ];
 
+
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
@@ -19,6 +22,7 @@ self.addEventListener('install', (event) => {
         })
     );
 });
+
 
 // servir desde red si se puede, si no, desde cache
 self.addEventListener('fetch', (event) => {
@@ -33,7 +37,6 @@ self.addEventListener('fetch', (event) => {
     );
 });
 
-
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
@@ -45,3 +48,56 @@ self.addEventListener('activate', (event) => {
         })
     );
 });
+*/
+
+const CACHE_NAME = `wornux-cache-${Date.now()}`;
+const STATIC_ASSETS = [
+    '/shortened',
+    '/shortened/dashboard',
+    '/css/quaters.css',
+    '/css/tailwind.css',
+    '/js/toggleSideMenu.js',
+    '/js/toggleAvatarMenu.js',
+    '/js/preview.js',
+    '/sw.js',
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(STATIC_ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then(keys =>
+                Promise.all(
+                    keys.filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
+                )
+            )
+        ])
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        fetch(event.request)
+            .then(networkRes => {
+                const resClone = networkRes.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+                return networkRes;
+            })
+            .catch(() =>
+                caches.match(event.request)
+            )
+    );
+});
+
+
